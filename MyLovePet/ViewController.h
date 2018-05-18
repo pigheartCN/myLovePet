dddcbc0e81f0e466a4dd26de97041bef3173ece2
zhuxin <txmac@pigheart.local>
2018-05-18 11:09:28 +0800

    Initial Commit

---
+++ MyLovePet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLovePet
+//
+//  Created by zhuxin on 2018/5/18.
+//  Copyright © 2018年 zhuxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyLovePet/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyLovePet
+//
+//  Created by zhuxin on 2018/5/18.
+//  Copyright © 2018年 zhuxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
