99d60d34ab62ac5ab44c978226d05d774dd8f730
zhuxin <txmac@pigheart.local>
2018-05-19 16:23:59 +0800

    添加融云

--- MyLovePet/AppDelegate.m
+++ MyLovePet/AppDelegate.m
@@ -13,6 +13,8 @@
 #import "PersonalViewController.h"
 #import "PetTabBarViewController.h"
 
+#import <RongIMKit/RongIMKit.h>
+
 @interface AppDelegate ()
 
 @end
@@ -22,6 +24,35 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //融云IM
+    [[RCIM sharedRCIM] initWithAppKey:@"0vnjpoad06ttz"];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSString *identifierForVendor = [[[UIDevice currentDevice].identifierForVendor UUIDString] stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    
+    NSMutableDictionary *paraDic = [NSMutableDictionary new];
+    [paraDic setObject:identifierForVendor forKey:@"userId"];
+    [paraDic setObject:@"testPig" forKey:@"name"];
+    [paraDic setObject:@"" forKey:@"portraitUri"];
+    
+    [manager POST:@"http://api.cn.ronghub.com/user/getToken.json" parameters:paraDic success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSDictionary *resultDic = (NSDictionary *)responseObject;
+        NSLog(@"resultDic:%@",resultDic);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"失败了哦！！！");
+    }];
+//    [[RCIM sharedRCIM] connectWithToken:@"YourTestUserToken"     success:^(NSString *userId) {
+//        NSLog(@"登陆成功。当前登录的用户ID：%@", userId);
+//    } error:^(RCConnectErrorCode status) {
+//        NSLog(@"登陆的错误码为:%ld", (long)status);
+//    } tokenIncorrect:^{
+//        //token过期或者不正确。
+//        //如果设置了token有效期并且token过期，请重新请求您的服务器获取新的token
+//        //如果没有设置token有效期却提示token错误，请检查您客户端和服务器的appkey是否匹配，还有检查您获取token的流程。
+//        NSLog(@"token错误");
+//    }];
+    //配置tabbar
     [self configNavigationTabbar];
     return YES;
 }
